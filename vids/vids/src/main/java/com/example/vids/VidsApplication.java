20b4ccaa3ab1c2c9ff00238f36a77517c3d37039
vids008 <vidswaghmare@gmail.com>
2019-06-14 22:45:58 +0530

    first commit

---
+++ vids/vids/src/main/java/com/example/vids/VidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidsApplication.class, args);
+	}
+
+}
---
+++ vids/vids/src/test/java/com/example/vids/VidsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.vids;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+
+
+public class VidsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
